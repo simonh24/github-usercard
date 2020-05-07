bba67887f4cbc497616f67df07080ac6c88abefb
Simon <simonh72498@gmail.com>
2020-05-07 16:27:15 -0400

    finished step 3, cardMaker

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,7 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get(`https://api.github.com/users/simonh24`)
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +50,51 @@ const followersArray = [];
       </div>
     </div>
 */
+
+
+function cardMaker(userAttrs) {
+  const {avatar_url, name, login, location, html_url, followers, following, bio} = userAttrs;
+
+  const card = document.createElement("div");
+  const imageURL = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const h3Name = document.createElement("h3");
+  const pUsername = document.createElement("p");
+  const pLocation = document.createElement("p");
+  const pProfile = document.createElement("p");
+  const linkPage = document.createElement("a");
+  const pFollowers = document.createElement("p");
+  const pFollowing = document.createElement("p");
+  const pBio = document.createElement("p");
+
+  card.appendChild(imageURL);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(h3Name);
+  cardInfo.appendChild(pUsername);
+  cardInfo.appendChild(pLocation);
+  cardInfo.appendChild(pProfile);
+  pProfile.appendChild(linkPage);
+  cardInfo.appendChild(pFollowers);
+  cardInfo.appendChild(pFollowing);
+  cardInfo.appendChild(pBio);
+
+  card.classList.add("card");
+  imageURL.setAttribute("src", avatar_url);
+  cardInfo.classList.add("card-info");
+  h3Name.classList.add("name");
+  h3Name.textContent = name;
+  pUsername.classList.add("username");
+  pUsername.textContent = login;
+  pLocation.textContent = location;
+  pProfile.textContent = "Profile: ";
+  linkPage.setAttribute("href", html_url);
+  linkPage.textContent = html_url;
+  pFollowers.textContent = `Followers: ${followers}`;
+  pFollowing.textContent = `Following: ${following}`;
+  pBio.textContent = `Bio: ${bio}`;
+
+  return card;
+}
 
 /*
   List of LS Instructors Github username's:
